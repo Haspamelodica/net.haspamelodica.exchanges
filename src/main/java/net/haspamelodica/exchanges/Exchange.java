5c7bcafbd41ef72cf795b478a980f70bd8fba2f0
Haspamelodica <melodicahaspa@gmail.com>
2023-07-04 21:41:41 +0200

    Implemented a pipe
    The reason for an own pipe is that java.io.Piped[In|Out]putStreams don't
    work properly with multiple threads.

--- src/main/java/net/haspamelodica/exchanges/Exchange.java
+++ src/main/java/net/haspamelodica/exchanges/Exchange.java
@@ -4,12 +4,7 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InterruptedIOException;
 import java.io.OutputStream;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.concurrent.atomic.AtomicReference;
 
 import net.haspamelodica.exchanges.util.AutoClosablePair;
 
@@ -40,43 +35,12 @@ public record Exchange(InputStream in, OutputStream out) implements AutoCloseabl
 	public static AutoClosablePair<Exchange, Exchange> openPiped() throws IOException
 	{
 		// java.io.Piped[In|Out]putStream's don't work with multiple threads (they cause random IOExceptions with message "Pipe broken").
-		AtomicReference<Socket> sock1Ref = new AtomicReference<>();
-		AtomicReference<IOException> sock1IOExceptionRef = new AtomicReference<>();
-		Thread sock1Thread;
-		Socket sock2;
-		try(ServerSocket server = new ServerSocket())
-		{
-			server.bind(null);
-			sock1Thread = new Thread(() ->
-			{
-				try
-				{
-					sock1Ref.set(new Socket(InetAddress.getLocalHost(), server.getLocalPort()));
-				} catch(IOException e)
-				{
-					sock1IOExceptionRef.set(e);
-				}
-			});
-			sock1Thread.start();
-			sock2 = server.accept();
-		}
-		try
-		{
-			sock1Thread.join();
-		} catch(InterruptedException e)
-		{
-			throw new InterruptedIOException(e.getMessage());
-		}
-		Socket sock1 = sock1Ref.get();
-		if(sock1 == null)
-		{
-			IOException sock1IOException = sock1IOExceptionRef.get();
-			if(sock1IOException != null)
-				throw sock1IOException;
-			throw new IOException("Unknown error while creating piped exchanges");
-		}
+		@SuppressWarnings("resource")
+		Pipe pipe1 = new Pipe();
+		@SuppressWarnings("resource")
+		Pipe pipe2 = new Pipe();
 		return new AutoClosablePair<>(
-				new Exchange(sock1.getInputStream(), sock1.getOutputStream()),
-				new Exchange(sock2.getInputStream(), sock2.getOutputStream()));
+				new Exchange(pipe1.in(), pipe2.out()),
+				new Exchange(pipe2.in(), pipe1.out()));
 	}
 }
---
+++ src/main/java/net/haspamelodica/exchanges/Pipe.java
@@ -0,0 +1,194 @@
+package net.haspamelodica.exchanges;
+
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InterruptedIOException;
+import java.io.OutputStream;
+import java.util.Objects;
+import java.util.concurrent.Semaphore;
+
+public class Pipe implements AutoCloseable
+{
+	private final Semaphore	writingReadySemaphore;
+	private final Semaphore	readingReadySemaphore;
+	private final Semaphore	readingFinishedSemaphore;
+
+	private boolean	closed;
+	private boolean	writtenSingleValue;
+	private byte	writtenValue;
+	private byte[]	writtenBuf;
+	private int		writtenOff;
+	private int		writtenLen;
+
+	public Pipe()
+	{
+		writingReadySemaphore = new Semaphore(1);
+		readingReadySemaphore = new Semaphore(0);
+		readingFinishedSemaphore = new Semaphore(0);
+	}
+
+	public InputStream in()
+	{
+		return new InputStream()
+		{
+			@Override
+			public int read() throws IOException
+			{
+				if(acquireIOAndCheckClosed(readingReadySemaphore))
+					return -1;
+
+				if(writtenSingleValue)
+				{
+					int result = writtenValue & 0xFF;
+					writingReadySemaphore.release();
+					return result;
+				}
+
+				int oldWrittenLen = writtenLen;
+				if(oldWrittenLen > 1)
+				{
+					writtenLen = oldWrittenLen - 1;
+					int result = writtenBuf[writtenOff ++] & 0xFF;
+					readingReadySemaphore.release();
+					return result;
+				}
+
+				int result = writtenBuf[writtenOff] & 0xFF;
+				writtenBuf = null;
+				readingFinishedSemaphore.release();
+				return result;
+			}
+
+			@Override
+			public int read(byte[] b, int off, int len) throws IOException
+			{
+				Objects.checkFromIndexSize(off, len, b.length);
+				if(len == 0)
+					return 0;
+				// from here: len > 1
+
+				if(acquireIOAndCheckClosed(readingReadySemaphore))
+					return -1;
+
+				if(writtenSingleValue)
+				{
+					b[off] = writtenValue;
+					writingReadySemaphore.release();
+					return 1;
+				}
+
+				int oldWrittenLen = writtenLen;
+				if(oldWrittenLen > len)
+				{
+					writtenLen = oldWrittenLen - len;
+					System.arraycopy(writtenBuf, writtenOff, b, off, len);
+					writtenOff += len;
+					readingReadySemaphore.release();
+					return len;
+				}
+
+				System.arraycopy(writtenBuf, writtenOff, b, off, oldWrittenLen);
+				writtenBuf = null;
+				readingFinishedSemaphore.release();
+				return oldWrittenLen;
+			}
+
+			@Override
+			public void close()
+			{
+				Pipe.this.close();
+			}
+		};
+	}
+
+	public OutputStream out()
+	{
+		return new OutputStream()
+		{
+			@Override
+			public void write(int b) throws IOException
+			{
+				if(acquireIOAndCheckClosed(writingReadySemaphore))
+					throw new EOFException();
+				writtenSingleValue = true;
+				writtenValue = (byte) b;
+				readingReadySemaphore.release();
+
+				// no need to wait for readingFinished
+			}
+
+			@Override
+			public void write(byte[] b, int off, int len) throws IOException
+			{
+				Objects.checkFromIndexSize(off, len, b.length);
+				if(acquireIOAndCheckClosed(writingReadySemaphore))
+					throw new EOFException();
+
+				if(len == 0)
+				{
+					writingReadySemaphore.release();
+					return;
+				}
+				// from here: len > 0
+
+				writtenSingleValue = false;
+				writtenBuf = b;
+				writtenOff = off;
+				writtenLen = len;
+				readingReadySemaphore.release();
+
+				// Make sure we're only returning when all bytes have been read.
+				// This is neccessary because otherwise the caller could modify the buffer.
+				if(acquireIOAndCheckClosed(readingFinishedSemaphore))
+				{
+					writtenBuf = null;
+					throw new EOFException();
+				}
+				writingReadySemaphore.release();
+			}
+
+			@Override
+			public void close()
+			{
+				Pipe.this.close();
+			}
+		};
+	}
+
+	private boolean acquireIOAndCheckClosed(Semaphore semaphore) throws InterruptedIOException
+	{
+		if(closed)
+			return true;
+
+		try
+		{
+			semaphore.acquire();
+		} catch(InterruptedException e)
+		{
+			throw new InterruptedIOException();
+		}
+		if(closed)
+		{
+			semaphore.release();
+			return true;
+		}
+
+		return false;
+	}
+
+	public Exchange asExchange()
+	{
+		return new Exchange(in(), out());
+	}
+
+	@Override
+	public void close()
+	{
+		closed = true;
+		writtenBuf = null;
+		writingReadySemaphore.release();
+		readingReadySemaphore.release();
+		readingFinishedSemaphore.release();
+	}
+}
