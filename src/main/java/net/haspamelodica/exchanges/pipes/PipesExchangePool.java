c89748773148b1240dfd7629e05c5ca39005878d
Haspamelodica <melodicahaspa@gmail.com>
2023-08-04 02:40:14 +0200

    PipesExchangePool now supports custom pipe creation

--- src/main/java/net/haspamelodica/exchanges/pipes/PipesExchangePool.java
+++ src/main/java/net/haspamelodica/exchanges/pipes/PipesExchangePool.java
@@ -1,6 +1,7 @@
 package net.haspamelodica.exchanges.pipes;
 
 import java.util.concurrent.SynchronousQueue;
+import java.util.function.Supplier;
 
 import net.haspamelodica.exchanges.Exchange;
 import net.haspamelodica.exchanges.ExchangePool;
@@ -9,17 +10,24 @@ import net.haspamelodica.exchanges.util.AutoCloseablePair;
 
 public class PipesExchangePool extends SimpleExchangePool
 {
+	private final Supplier<AutoCloseablePair<Exchange, Exchange>> openPiped;
+
 	private final Client client;
 
 	public PipesExchangePool()
 	{
+		this(Exchange::openPiped);
+	}
+	public PipesExchangePool(Supplier<AutoCloseablePair<Exchange, Exchange>> openPiped)
+	{
+		this.openPiped = openPiped;
 		this.client = new Client();
 	}
 
 	@Override
 	protected Exchange createExchangeInterruptible() throws InterruptedException
 	{
-		AutoCloseablePair<Exchange, Exchange> pipe = Exchange.openPiped();
+		AutoCloseablePair<Exchange, Exchange> pipe = openPiped.get();
 		client.put(pipe.a());
 		return pipe.b();
 	}
